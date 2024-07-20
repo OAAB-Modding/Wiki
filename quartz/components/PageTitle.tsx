0b27dd66ecd8c0c28654e1a4e0df991d2b247825
Greatness7 <Greatness7@gmail.com>
2024-07-20 16:13:36 -0400

    Quartz v4
    Commit Hash: 87f2b0c3278e6b9fd438922c3f1e37194ceb8c92

---
+++ quartz/components/PageTitle.tsx
@@ -0,0 +1,32 @@
+import { pathToRoot } from "../util/path"
+import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
+import { classNames } from "../util/lang"
+import { joinSegments } from "../util/path"
+import { i18n } from "../i18n"
+
+const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzComponentProps) => {
+  const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
+  const baseDir = pathToRoot(fileData.slug!)
+  const iconPath = joinSegments(baseDir, "static/logo.png")
+  return (
+    <h1 class={classNames(displayClass, "page-title")}>
+      <a href={baseDir}>
+        <img class="Logo" src={iconPath} alt={title} />
+      </a>
+    </h1>
+  )
+}
+
+PageTitle.css = `
+.page-title {
+  margin: 0;
+}
+.Logo {
+max-height: 650px;
+min-height: 50px;
+max-width: 240px;
+min-width: 50px;
+}
+`
+
+export default (() => PageTitle) satisfies QuartzComponentConstructor
