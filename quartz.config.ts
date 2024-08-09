945ec6be6a774038eaa1eb51a58b8f283b15465f
MassiveJuice <122503558+MasssiveJuice08@users.noreply.github.com>
2024-08-10 06:20:20 +1200

    Update quartz.config.ts
    - Updated baseurl
    - removed emoji from 'title'

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,14 +8,14 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 OAAB Wiki",
+    pageTitle: "OAAB Wiki",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "oaab.dev",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
